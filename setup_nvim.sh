2848c3cbdd093706a65832c2af2f13e4f16fb8f4
bygu4 <sacha05bugaev@gmail.com>
2026-07-28 01:41:39 +0300

    nvim setup: create ~/.config/nvim if not present

--- setup_nvim.sh
+++ setup_nvim.sh
@@ -1,13 +1,14 @@
 #! /bin/sh
 set -e
 
+mkdir -p ~/.config/nvim
+mkdir -p ~/.local/share/nvim/undo/
+
 echo 'set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
 source ~/.vimrc
 
 set undofile
 set undodir=~/.local/share/nvim/undo/' > ~/.config/nvim/init.vim
-
-mkdir -p ~/.local/share/nvim/undo/
 
 echo "Done!"
